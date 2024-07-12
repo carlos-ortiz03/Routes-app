bbe188355e7ade2052846214a455cfae5b6e0668
carlosortiz <carlos.a.ortiz03@gmail.com>
2024-07-11 23:20:25 -0500

    got it working in dist file

--- Frontend/src/components/LandingPage/VideoBackground.tsx
+++ Frontend/src/components/LandingPage/VideoBackground.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import background from "/landing_vid.mp4";
 
 const VideoBackground: React.FC = () => {
   return (
@@ -9,7 +10,7 @@ const VideoBackground: React.FC = () => {
         muted
         className="absolute top-1/2 left-1/2 w-full h-full object-cover transform -translate-x-1/2 -translate-y-1/2 z-[-1]"
       >
-        <source src="../../landing_vid.mp4" type="video/mp4" />
+        <source src={background} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-0"></div>
--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
   },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  base: "./", // Ensure this is set correctly for relative paths
 });
